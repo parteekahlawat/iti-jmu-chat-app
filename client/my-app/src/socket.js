84f16bc0e8699c5106be46ce2864dec20626e32d
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2024-04-10 15:04:06 +0530

    updated socket  url

--- client/my-app/src/socket.js
+++ client/my-app/src/socket.js
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("https://12ac7067-c19d-4e04-b4b2-2c073686dea3-00-nqq2yoye0g0b.picard.replit.dev");
+export const socket = io("https://d99add0a-89a1-4e30-b9c6-4dc98583bb42-00-1c1py72num98m.riker.replit.dev");
